52339cf7285f800408f8754cd2580ca9e82059cd
Simon Jones <sipajones@gmail.com>
2024-10-25 14:20:33 +0100

    Add a heartbeat

--- halloween_2024.ino
+++ halloween_2024.ino
@@ -14,46 +14,79 @@ struct LEDState {
 
 // Create an array of LEDState objects
 LEDState states[] = {
-  {CRGB(128, 255, 0), 10, 6000}, // Bright orange
-  {CRGB(25, 50, 0), 600, 1000},  // Mid brightness orange
-  {CRGB(10, 20, 0), 600, 1000},  // Low brightness orange
-  {CRGB(0, 255, 0), 5000, 5000},  // Red
-  {CRGB::Black, 10, 2000}        // Off
+  {CRGB(240, 255, 0), 1000, 4000}, // Bright orange
+  {CRGB(30, 50, 0), 600, 800},  // Mid brightness orange
+//  {CRGB(10, 20, 0), 600, 1000},  // Low brightness orange
+  {CRGB::Black, 100, 500}        // Off
 };
 
 // Define weights for each state
-int weights[] = {40, 25, 24, 1, 10}; // Red has a very low weight
+int weights[] = {40, 30, 20}; 
 
 void setup() {
   FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
 }
 
 void loop() {
-  // Calculate the total weight
-  int totalWeight = 0;
-  for (int i = 0; i < 4; i++) {
-    totalWeight += weights[i];
+
+  int number_of_yellow_on_off_changes_before_heartbeat = 30;
+  int number_of_heartbeats = 10;
+
+  for (int k = 0; k < number_of_yellow_on_off_changes_before_heartbeat; k++) {
+    int totalWeight = 0;
+    for (int i = 0; i < 3; i++) {
+      totalWeight += weights[i];
+    }
+    int randomWeight = random(0, totalWeight);
+    int stateIndex = 0;
+    int cumulativeWeight = weights[0];
+    while (randomWeight >= cumulativeWeight) {
+      stateIndex++;
+      cumulativeWeight += weights[stateIndex];
+    }
+    LEDState currentState = states[stateIndex];
+    for(int i = 0; i < NUM_LEDS; i++) {
+      leds[i] = currentState.color;
+    }
+    FastLED.show();
+    delay(random(currentState.minTime, currentState.maxTime));
   }
 
-  // Generate a random number within the total weight
-  int randomWeight = random(0, totalWeight);
+  // Heartbeat
+  CRGB lightRed = CRGB(0, 255, 0);
+  CRGB midRed = CRGB(0, 110, 0);
+  CRGB deepRed = CRGB(0, 40, 0);
 
-  // Select the state based on the random weight
-  int stateIndex = 0;
-  int cumulativeWeight = weights[0];
-  while (randomWeight >= cumulativeWeight) {
-    stateIndex++;
-    cumulativeWeight += weights[stateIndex];
-  }
+  for (int k = 0; k < number_of_heartbeats; k++) {
 
-  LEDState currentState = states[stateIndex];
-  
-  // Apply the selected state
-  for(int i = 0; i < NUM_LEDS; i++) {
-    leds[i] = currentState.color;
-  }
-  FastLED.show();
-  
-  // Wait for a random interval within the selected state's time range
-  delay(random(currentState.minTime, currentState.maxTime));
+    for (int j = 16; j >= 0; j--) {
+      for (int i = 0; i < NUM_LEDS; i++) {
+        leds[i] = blend(midRed, deepRed, j * 16);
+      }
+      FastLED.show();
+      delay(0);
+    }
+    for (int j = 0; j < 128; j++) {
+      for (int i = 0; i < NUM_LEDS; i++) {
+        leds[i] = blend(midRed, deepRed, j * 2);
+      }
+      FastLED.show();
+      delay(1.8);
+    }
+    for (int j = 16; j >= 0; j--) {
+      for (int i = 0; i < NUM_LEDS; i++) {
+        leds[i] = blend(lightRed, deepRed, j * 16);
+      }
+      FastLED.show();
+      delay(0.00); 
+    }
+    for (int j = 0; j < 128; j++) {
+      for (int i = 0; i < NUM_LEDS; i++) {
+        leds[i] = blend(lightRed, deepRed, j * 2);
+      }
+      FastLED.show();
+      delay(4); 
+    }
+    
+  } 
 }
